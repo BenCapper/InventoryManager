edf93ce217917c0aac36a30843640d04190c0a5a
Ben Capper <Bencapper19@gmail.com>
2022-01-28 15:48:05 +0000

    Create building model
    Implement the building model and interface methods
    
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/wit/inventorymanager/models/BuildingStore.kt
+++ app/src/main/java/org/wit/inventorymanager/models/BuildingStore.kt
@@ -1,4 +1,9 @@
 package org.wit.inventorymanager.models
 
 interface BuildingStore {
+    fun findAll(): List<BuildingModel>
+    fun create(building: BuildingModel)
+    fun update(building: BuildingModel)
+    fun delete(building: BuildingModel)
+    fun filterBuildings(buildingName: String): List<BuildingModel>
 }
