0f0b363c8c36eb78271083a74543706b7b47a48a
Ben Capper <Bencapper19@gmail.com>
2022-01-28 14:51:42 +0000

    Initial commit
    Add file structure
    
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

---
+++ app/src/main/java/org/wit/inventorymanager/main/MainApp.kt
@@ -0,0 +1,14 @@
+package org.wit.inventorymanager.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.wit.inventorymanager.R
+
+class MainApp : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
