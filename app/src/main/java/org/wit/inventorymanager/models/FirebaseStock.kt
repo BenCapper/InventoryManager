fb54e9396722fa6250bf0f7b2bcf1ba3a4cc50de
Ben Capper <Bencapper19@gmail.com>
2022-01-28 16:04:27 +0000

    Implement Firebase models
    Create crud functionality for a Firebase
    database for building and stock models
    
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/wit/inventorymanager/models/FirebaseStock.kt
+++ app/src/main/java/org/wit/inventorymanager/models/FirebaseStock.kt
@@ -1,4 +1,42 @@
 package org.wit.inventorymanager.models
 
-class FirebaseStock {
+import com.google.firebase.database.FirebaseDatabase
+import java.util.*
+
+class FirebaseStock : StockStore {
+
+    var stock = mutableListOf<StockModel>()
+    private val db = FirebaseDatabase.getInstance().reference.child("Stock")
+
+
+    override fun findAll(): MutableList<StockModel> {
+        return stock
+    }
+
+
+    fun generateRandomStockId(): Long {
+        return Random().nextLong()
+    }
+
+    override fun create(stock: StockModel) {
+        db.child(stock.id.toString()).setValue(stock)
+    }
+
+
+    override fun update(stock: StockModel) {
+        db.child(stock.id.toString()).setValue(stock)
+
+    }
+
+    override fun delete(stock: StockModel) {
+        db.child(stock.id.toString()).removeValue()
+
+    }
+
+
+    override fun filterStock(id: Long): List<StockModel> {
+        return stock.filter { item -> item.branch != id }
+    }
 }
+
+
