6e09d2c56353bd2d8fe161888717cc8a856a5f7d
Ben Capper <Bencapper19@gmail.com>
2022-01-29 19:38:05 +0000

    Implement template logic for BuildingFragment
    Refactor donations example
    Begin setButtonListener logic
    
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/wit/inventorymanager/fragments/BuildingFragment.kt
+++ app/src/main/java/org/wit/inventorymanager/fragments/BuildingFragment.kt
@@ -1,60 +1,73 @@
 package org.wit.inventorymanager.fragments
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import org.wit.inventorymanager.BuildingFragment
 import org.wit.inventorymanager.R
+import org.wit.inventorymanager.databinding.FragmentBuildingBinding
+import org.wit.inventorymanager.main.InventoryApp
+import org.wit.inventorymanager.models.BuildingModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+lateinit var app: InventoryApp
+private var _fragBinding: FragmentBuildingBinding? = null
+private val fragBinding get() = _fragBinding!!
+private var building = BuildingModel()
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BuildingFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class BuildingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        app = activity?.application as InventoryApp
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_building, container, false)
+
+        _fragBinding = FragmentBuildingBinding.inflate(inflater, container, false)
+        val root = fragBinding.root
+        activity?.title = getString(R.string.action_location)
+
+        return root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    private fun setButtonListener(layout: FragmentBuildingBinding) {
+        layout.btnAdd.setOnClickListener {
+            building.name = layout.buildingName.text.toString()
+            building.address = layout.buildingAddress.text.toString()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_building, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,
+            requireView().findNavController()) || super.onOptionsItemSelected(item)
+    }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BuildingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
             BuildingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+                arguments = Bundle().apply {}
             }
     }
 }
