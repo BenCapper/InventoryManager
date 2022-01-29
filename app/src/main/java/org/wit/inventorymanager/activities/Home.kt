163e74e51b4c26362b83bbd4c2c9e5140976feb4
Ben Capper <Bencapper19@gmail.com>
2022-01-29 19:08:03 +0000

    Fragment setup
    Create fragments for building and stock lists
    Update the theme to black/orange which works in dark/light modes
    Update buildingFragment design
    Enable viewBinding in build.gradle (module)
    Introduce Home.kt override functions
    Temporarily disable Firebase object arrays
    
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/wit/inventorymanager/activities/Home.kt
+++ app/src/main/java/org/wit/inventorymanager/activities/Home.kt
@@ -1,6 +1,41 @@
 package org.wit.inventorymanager.activities
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.*
+import org.wit.inventorymanager.databinding.HomeBinding
+import org.wit.inventorymanager.R
 
 class Home : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var homeBinding : HomeBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        homeBinding = HomeBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
+        drawerLayout = homeBinding.drawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        val navView = homeBinding.navView
+        navView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.buildingFragment, R.id.reportFragment,), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 }
