1472caa852cad51e7e47aecf35ca5f37d564ca5a
Ben Capper <Bencapper19@gmail.com>
2022-02-09 16:31:36 +0000

    Reset building values after add/update
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/wit/inventorymanager/fragments/BuildingFragment.kt
+++ app/src/main/java/org/wit/inventorymanager/fragments/BuildingFragment.kt
@@ -59,34 +59,32 @@ class BuildingFragment : Fragment() {
             building = bundle?.getParcelable("editBuild")!!
 
 
-            if (building.id.toString() !== "0" && building.id.toString() !== null ){
+            if (building.id !== (0).toLong()){
                 id = building.id
                 fragBinding.btnAdd.setText(R.string.up_loc)
             }
-            if (building.name !== ""){
+            if (building.name != ""){
                 fragBinding.buildingName.setText(building.name)
             }
-            if (building.address !== ""){
+            if (building.address != ""){
                 fragBinding.buildingAddress.setText(building.address)
             }
-            if (building.phone !== ""){
+            if (building.phone != ""){
                 fragBinding.editTextPhone.setText(building.phone)
             }
-            if (building.image !== ""){
+            if (building.image != ""){
                 Picasso.get()
                     .load(Uri.parse(building.image))
                     .into(fragBinding.buildingImage)
                 fragBinding.chooseImage.setText(R.string.img_ch)
             }
-        }
-
-        if(building.image !== ""){
-            Picasso.get()
-                .load(Uri.parse(building.image))
-                .into(fragBinding.buildingImage)
-            fragBinding.chooseImage.setText(R.string.img_ch)
+            else{
+                fragBinding.buildingImage.setImageURI(null)
+            }
 
         }
+
+
 
         requireActivity().onBackPressedDispatcher.addCallback(this) {
         requireActivity().findNavController(R.id.nav_host_fragment).navigate(R.id.action_buildingFragment_to_buildingListFragment)
@@ -133,9 +131,7 @@ class BuildingFragment : Fragment() {
     private fun setButtonListener(layout: FragmentBuildingBinding) {
         layout.btnAdd.setOnClickListener {
 
-            if (building.id.toString().length == 1) {
-                building.id = Random().nextLong()
-            }
+
             building.name = layout.buildingName.text.toString()
             building.address = layout.buildingAddress.text.toString()
             building.phone = layout.editTextPhone.text.toString()
@@ -146,14 +142,15 @@ class BuildingFragment : Fragment() {
                 toast(R.string.loc_address)
             }  else if (building.phone.isEmpty()) {
                 toast(R.string.loc_phone)
-            }  else if ( building.image == "null") {
+            }  else if ( building.image == "") {
                 toast(R.string.loc_img)
             }else {
-                if (id.toString().length > 1){
-                    app.builds.update(building)
+                if (building.id.toString().length == 1){
+                    building.id = Random().nextLong()
+                    app.builds.create(building)
                 }
                 else {
-                    app.builds.create(building)
+                    app.builds.update(building)
                 }
                 Timber.i(building.toString())
 
@@ -164,7 +161,12 @@ class BuildingFragment : Fragment() {
                 building.name = ""
                 building.phone = ""
                 building.address = ""
-                building.image = "null"
+                building.image = ""
+                building.id = 0
+                building.zoom = 0f
+                building.lat = (0).toDouble()
+                building.lng = (0).toDouble()
+
 
                 it.findNavController()
                     .navigate(R.id.action_buildingFragment_to_buildingListFragment)
